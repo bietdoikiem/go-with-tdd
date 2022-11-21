8dc74e0d1383cbbbb6a6357b33f93735946a4684
minhnguyenphamquoc <Minh_Nguyen@mckinsey.com>
2022-11-21 21:58:59 +0700

    feat: add select

---
+++ selection/racer.go
@@ -0,0 +1,37 @@
+package selection
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(urlA, urlB string) (winner string, err error) {
+	return ConfigurableRacer(urlA, urlB, tenSecondTimeout)
+}
+
+// ConfigurableRacer returns the winner's URL based on response time of
+// urlA and urlB
+func ConfigurableRacer(urlA, urlB string, timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(urlA):
+		return urlA, nil
+	case <-ping(urlB):
+		return urlB, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", urlA, urlB)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+
+	return ch
+}
---
+++ selection/racer_test.go
@@ -0,0 +1,59 @@
+package selection
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("return url of the server with the faster response time", func(t *testing.T) {
+
+		slowServer := makeDelayTestServer(12 * time.Millisecond)
+		fastServer := makeDelayTestServer(1 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q ", got, want)
+		}
+	})
+
+	t.Run("return an error if a server doesn't respond within 10 seconds", func(t *testing.T) {
+		slowServer := makeDelayTestServer(15 * time.Millisecond)
+		fastServer := makeDelayTestServer(11 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		_, err := ConfigurableRacer(slowURL, fastURL, 10*time.Millisecond)
+
+		if err == nil {
+			t.Error("expect an error but didn't get one")
+		}
+
+	})
+}
+
+// makeDelayTestServers make a server with delay for unit testing purpose
+func makeDelayTestServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
